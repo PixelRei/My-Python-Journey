82fd199d01a4246ea39313635fe79f6b522f12bb
PixelRei <playboi.rei17@gmail.com>
2025-07-14 16:50:42 +0200

    Add files via upload

---
+++ strings/OtherMethods.py
@@ -0,0 +1,12 @@
+#merging elements of a list
+lista = ["Aldo", "Giovanni", "Giacomo"]
+char = "&"
+print(char.join(lista))
+
+#to check if a string starts with something
+string = "hello G"
+print(string.startswith("hello")) #this returns true
+#endswith() works in the same way
+
+#to make the first char an Uppercase we use capitalize()
+print(string.capitalize())
