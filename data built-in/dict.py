b9e42c0d094c2df243588acd4b954a7719aca44f
PixelRei <playboi.rei17@gmail.com>
2025-07-15 15:17:51 +0200

    Add files via upload

---
+++ data built-in/dict.py
@@ -0,0 +1,34 @@
+#a dictionary consist of key and value
+d = {"one": 1, "two": 2, "three" : 3}
+print(d["one"])
+print(d) #if the key doesn't exist it return KeyError
+if 'one' in d: #or "not in"
+    print("presente")
+#to overwrite or delete
+d['one'] = 1.1
+del d['three']
+print(d)
+
+#returning the elements in a tuple
+print(d.items())
+
+#returning the keys
+print(d.keys())
+
+#and the values
+print(d.values())
+
+#returning a copy of the dictionary
+copy_d = d.copy()
+
+#adding elements to the dictionary
+d1 = {
+    "a": 10,
+    "b": 20,
+    "c": 30
+}
+d.update(d1)
+print(d)
+
+#removing all the elements
+d.clear()
