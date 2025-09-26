77f0dc186b0e66d1e0f974f6869509e0adfb58c7
PixelRei <playboi.rei17@gmail.com>
2025-09-26 22:33:34 +0200

    Add files via upload

--- objects/test.py
+++ objects/test.py
@@ -11,9 +11,13 @@ class Calc:
         self.b = b
     def addition(self):
         print(self.a + self.b)
+    @staticmethod #if you want to create a static method in a class
+    def addNumbers(a, b):
+        print(a + b)
 
 number = Calc(5, 10)
 number.addition()
+number.addNumbers(20, 30)
 
 greet = Greet("Hiiiiii")
 greet.hello()
